c1f7ccfd5cfca4650b0df2ed6bcd1e56a27af5b5
Jan Carpentier <jan@transamte.eu>
2021-05-31 22:32:06 +0200

    export testing data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import Shipment from "./controller/shipment";
+// testing data
+import { shippoShipmentTesting } from "./data/shippo";
 
 const { rest } = require("./functions/rest");
 const { gqlResolve } = require("./functions/graphql");
 
-export { Shipment, rest, gqlResolve };
+export { Shipment, rest, gqlResolve, shippoShipmentTesting };
